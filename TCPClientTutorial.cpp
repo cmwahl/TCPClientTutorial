019e41680ad0ccc6a74653cb3cd295b5b2896c3e
Christopher Wahl <cmwahl@hotmail.com>
2020-12-10 16:20:26 -0600

    Add project files.

---
+++ TCPClientTutorial.cpp
@@ -0,0 +1,13 @@
+﻿// TCPClientTutorial.cpp : Defines the entry point for the application.
+//
+
+#include "TCPClientTutorial.h"
+#include "w"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Client." << endl;
+	return 0;
+}
